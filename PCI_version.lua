2cdb69e9a7616477d3f0b962c469e4d93c53c2d1
root <root@node0.snabbtesting.contools-pg0.wisc.cloudlab.us>
2019-09-22 19:51:47 -0500

    Uploading before transferring to new nodes.

--- PCI_version.lua
+++ PCI_version.lua
@@ -1,5 +1,7 @@
 module(..., package.seeall)
 
+Sender = {}
+
 local link     = require("core.link")
 local app      = require("core.app")
 local lib      = require("core.lib")
@@ -10,12 +12,11 @@ local ipv4     = require("lib.protocol.ipv4")
 local udp      = require("lib.protocol.udp")
 local datagram = require("lib.protocol.datagram")
 local raw_sock = require("apps.socket.raw")
+local pci      = require("lib.hardware.pci")
 
 local ffi =      require("ffi")
 local C = ffi.C
 
-
-Sender = {}
 
 function Sender:new(args)
 	print("Hi! You made it to Sender:new()!")
@@ -38,8 +39,8 @@ function Sender:new(args)
 	{
 		ether = ethernet:new(
 		{
-			src = ethernet:pton("90:e2:ba:b3:75:bd"),
-			dst = ethernet:pton("90:e2:ba:b3:ba:09"),
+			src = self._mac,
+			dst = ethernet:pton("90:e2:ba:b3:ba:08"),
 			type = 0x0800
 		}),
 		ip = ipv4:new(
@@ -79,33 +80,51 @@ end
 
 function Sender:pull()
 	assert(self.output.output, "No compatible output port found.")
+	-- print("Sending packet!")
 	link.transmit(self.output.output, self:gen_packet())
+end
+
+function test_ports(app, name)
+	assert(app.input, "No input port found for " .. name .. ".")
+	assert(app.output, "No output port found for " .. name .. ".")
 end
 
 
 function run (args)
         if not (#args == 4) then
-                -- print("Usage: send_data <data> <PCI addr> <interface> <ether> <IP> <port>")
-		print("Usage: SnabbUDP <data> <IF> <IP> <port>")
+		print("Usage: SnabbUDP <data> <PCI> <IP> <port>")
 		print("       data : File containing comma-separated numbers (e.g. 10,12.4,32)")
-		print("       IF   : Interface for packet(s) to be sent on")
-		print("       port : Port of the receiving user")
+		print("       PCI  : PCI Address linked with the interface to send packets on")
 		print("       IP   : IP Address of receiving user")
+		print("       port : Port of the receiving user")
                 main.exit(1)
         end
         
 	local data_file = args[1]
-        local IF = args[2]
+	local pci_addr = args[2]
 	local ip_dst = args[3]
 	local port = args[4]
-	
-	local c = config.new()
-	local RawSocket = raw_sock.RawSocket
-	config.app(c, "server", RawSocket, IF)
-	config.app(c, "sender", Sender, {data=data_file, ip_dst=ip_dst, port=tonumber(port)}) 
-	config.link(c, "sender.output->server.rx")
-	config.link(c, "server.rx->server.tx")
 
+	-- Init config for engine
+	local c = config.new()
+	
+	-- Resolving input/output given PCI address
+	local device_info = pci.device_info(pci_addr)
+	if device_info then
+		config.app(c, "nic", require(device_info.driver).driver, 
+		{
+			pciaddr = pci_addr,
+			mtu = 1500,
+		})
+		input, output = "nic."..device_info.rx, "nic."..device_info.tx
+	else
+		fatal(("Incompatible device from PCI %s"):format(pci_addr))
+	end
+
+	config.app(c, "sender", Sender, {data=data_file, ip_dst=ip_dst, port=tonumber(port)}) 
+
+	config.link(c, "sender.output ->" .. output)
+	
 	engine.configure(c)
         engine.main({report = {showlinks=true}, duration = 1})
 end
